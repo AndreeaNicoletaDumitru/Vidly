f63258ba21625ca4288a5082bd1758c06d234b11
Andreea Dumitru <Andreea Dumitru@BEAST>
2017-04-21 21:47:22 +0300

    Ceate random page. added new bootstrap layout - lumen.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movies = new Movie() { Name = "Shrek!" };
+
+            return View(movies);
+        }
+    }
+}
