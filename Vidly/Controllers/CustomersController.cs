a462f2c8aa86a01d4c758843bb9b5e6194b48717
Andreea Dumitru <Andreea Dumitru@BEAST>
2017-04-24 01:01:31 +0300

    Show multiple columns in customer. Added eager loading

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
@@ -21,7 +22,7 @@ namespace Vidly.Controllers
         // GET: Customers
         public ViewResult Index()
         {
-            var customers = context.Customers;
+            var customers = context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
